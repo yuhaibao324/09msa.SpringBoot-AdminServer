af95d69df80f5f6a7ab281487f98ce84b0aaf789
何惠民 <huimin.he@yi23.net>
2017-09-05 00:19:53 +0800

    :wrench:Add admin server configuration

--- src/main/java/cn/com/hellowood/admin/AdminApplication.java
+++ src/main/java/cn/com/hellowood/admin/AdminApplication.java
@@ -1,9 +1,14 @@
 package cn.com.hellowood.admin;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@EnableAutoConfiguration
+@Configuration
+@EnableAdminServer
 public class AdminApplication {
 
 	public static void main(String[] args) {
