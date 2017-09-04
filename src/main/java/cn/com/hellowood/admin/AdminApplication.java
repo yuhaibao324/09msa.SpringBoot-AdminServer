085b3191279a830d673766afe1360f470a8bcedb
何惠民 <huimin.he@yi23.net>
2017-09-05 00:17:50 +0800

    :tada:Initialize project

---
+++ src/main/java/cn/com/hellowood/admin/AdminApplication.java
@@ -0,0 +1,12 @@
+package cn.com.hellowood.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminApplication.class, args);
+	}
+}
